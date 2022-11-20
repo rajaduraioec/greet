8c141a4b506fba675f93aab7bf1b2d18fd1a5fd4
Rajadurai <rajaduraioec@gmail.com>
2022-11-20 18:32:52 +0000

    Initial Commit

---
+++ src/Greetr.php
@@ -0,0 +1,14 @@
+<?php
+
+namespace Rajaduraioec\Greet;
+
+
+
+class Greetr{
+
+    public function greet(String $sName)
+    {
+        return 'Hi ' . $sName . '! How are you doing today?';
+    }
+
+}
